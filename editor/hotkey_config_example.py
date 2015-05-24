0539f76780830c2df35819c8b4b4e6da7c0e46b2
Albert Visser <albert@lemoncurry>
2015-05-24 13:12:45 +0200

    added config example

---
+++ editor/hotkey_config_example.py
@@ -0,0 +1,15 @@
+"""
+This file contains some program wide settings
+"""
+# supported tools
+# Each entry is a tuple containing the name of the tool and the full path to a
+# csv file, where the key definitions are stored and also some settings specific
+# to the tool.
+plugins = [
+    ("Total Commander", "/home/albert/projects/hotkeys/csv_examples/TC_hotkeys.csv"),
+    ]
+
+# interface language
+# name (not full path) of the current language file
+LANG='english.lng'
+
