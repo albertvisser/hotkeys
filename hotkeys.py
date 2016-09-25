02c82cf2b2546932e0037169a05f16a5c5496943
Albert Visser <albert@lemoncurry>
2016-09-25 11:21:57 +0200

    switched to pyqt5 version

--- hotkeys.py
+++ hotkeys.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
-
-from editor.hotkeys_qt import main
+## from editor.hotkeys_wx import main
+## from editor.hotkeys_qt import main
+from editor.hotkeys_qt5 import main
 main()
