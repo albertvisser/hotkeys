35416387ed36243791570134ea89026c17d6a16b
Albert Visser <albert.visser@gmail.com>
2020-04-09 23:05:35 +0200

    quick check toegevoegd als aanroep argument

--- start.py
+++ start.py
@@ -2,11 +2,16 @@
 """A Manager for all your applications' hotkeys
 """
 import argparse
-from editor.main import Editor
+from editor.main import Editor, quick_check
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('-c', '--config', dest='conf',
                     help='name of module with config parameters (without .py'
                          ' extension)')
+parser.add_argument('-q', '--quick-check', dest='check',
+                    help='name of CSV file to check without starting up the GUI')
 args = parser.parse_args()
-Editor(args)
+if args.check:
+    quick_check(args.check)
+else:
+    Editor(args)
