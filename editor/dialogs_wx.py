746a216b9ec4ed8d74efb3f0701c4a298c279f9c
Albert Visser <albert.visser@gmail.com>
2020-04-11 11:42:17 +0200

    typo

--- editor/dialogs_wx.py
+++ editor/dialogs_wx.py
@@ -915,7 +915,7 @@ class CompleteDialog(wx.Dialog):
         self.p0list.SetRowLabelSize(20)
 
         for ix, row in enumerate((shared.get_text(self.parent, 'C_CMD'), (280),
-                                  shared.get_text(self.parent. 'C_DESC'), (400))):
+                                  shared.get_text(self.parent, 'C_DESC'), (400))):
             self.p0list.SetColLabelValue(ix, row[0])
             self.p0list.SetColSize(ix, row[1])
         # hdr.setStretchLastSection(True)
