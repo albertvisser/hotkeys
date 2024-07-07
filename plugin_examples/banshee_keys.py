33f27b3c42d5ccda87b57433e3ca4061da8237d8
Albert Visser <albert.visser@gmail.com>
2024-07-07 11:29:59 +0200

    naamswijziging

--- plugin_examples/banshee_keys.py
+++ plugin_examples/banshee_keys.py
@@ -3,5 +3,5 @@
 import functools
 import editor.plugins.gtkaccel_keys as gk
 F_KEYS = 'BNSH_KEYS'
-buildcsv = functools.partial(gk.buildcsv, F_KEYS)
+build_data = functools.partial(gk.build_data, F_KEYS)
 add_extra_attributes = gk.add_extra_attributes
