99ca7b9e9375a4a67686747a90517d0a0b15367b
Albert Visser <albert.visser@gmail.com>
2025-11-13 11:20:19 +0100

    overbodige regels verwijderd

--- plugin_examples/dckeys_wx.py
+++ plugin_examples/dckeys_wx.py
@@ -7,10 +7,10 @@ def layout_extra_fields(win, layout):
     """add the extra fields to the layout
     """
     sizer2 = wx.FlexGridSizer(2, 2, 2)
-    line = 0
+    # line = 0
     sizer2.Add(win.lbl_parms, 0, wx.ALIGN_CENTER_VERTICAL)
     sizer2.Add(win.txt_parms, 1, wx.EXPAND)
-    line += 1
+    # line += 1
     sizer2.Add(win.lbl_controls, 0, wx.ALIGN_CENTER_VERTICAL)
     sizer2.Add(win.cmb_controls, 1, wx.EXPAND)
     layout.Add(sizer2, 0, wx.LEFT, 5)
