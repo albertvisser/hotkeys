d7d37b9f3979058f86a595d0b5b31e9407653268
Albert Visser <albert.visser@gmail.com>
2025-11-13 11:20:46 +0100

    wx testmodules voor plugins afgemaakt

--- unittests/test_dckeys_wx.py
+++ unittests/test_dckeys_wx.py
@@ -1,7 +1,21 @@
-from editor.plugins import dckeys_wx as wxtestee
+import types
+from editor.plugins import dckeys_wx as testee
 from mockgui import mockwxwidgets as mockwx
 
 
-def _test_layout_extra_fields_wx(monkeypatch, capsys):
+def test_layout_extra_fields(monkeypatch, capsys):
     """testmethode voor dckey_wx.layout_extra_fields
     """
+    monkeypatch.setattr(testee.wx, 'FlexGridSizer', mockwx.MockFlexGridSizer)
+    layout = mockwx.MockBoxSizer('')
+    win = types.SimpleNamespace(lbl_parms='parms', txt_parms='parmstxt', lbl_controls='controls',
+                                cmb_controls='combobox')
+    testee.layout_extra_fields(win, layout)
+    assert capsys.readouterr().out == (
+            "called BoxSizer.__init__ with args ('',)\n"
+            "called FlexGridSizer.__init__ with args (2, 2, 2) {}\n"
+            f"called FlexGridSizer.Add with args <item> (0, {testee.wx.ALIGN_CENTER_VERTICAL})\n"
+            f"called FlexGridSizer.Add with args <item> (1, {testee.wx.EXPAND})\n"
+            f"called FlexGridSizer.Add with args <item> (0, {testee.wx.ALIGN_CENTER_VERTICAL})\n"
+            f"called FlexGridSizer.Add with args <item> (1, {testee.wx.EXPAND})\n"
+            f"called  sizer.Add with args <item> (0, {testee.wx.LEFT}, 5)\n")
--- unittests/test_vikeys_wx.py
+++ unittests/test_vikeys_wx.py
@@ -1,7 +1,30 @@
+import types
 from editor.plugins import vikeys_wx as testee
 from mockgui import mockwxwidgets as mockwx
 
 
-def test_layout_extra_fields_topline_wx(monkeypatch, capsys):
-    """unittest for vikeys_wx.layout_extra-fields_topline
+def test_layout_extra_fields_topline(monkeypatch, capsys):
+    """unittest for vikeys_wx.layout_extra_fields_topline
     """
+    monkeypatch.setattr(testee.wx, 'BoxSizer', mockwx.MockBoxSizer)
+    layout = mockwx.MockBoxSizer('')
+    win = types.SimpleNamespace(pre_parms_label='pre_label', pre_parms_text='pre_text',
+                                post_parms_label='post_label', post_parms_text='post_text',
+                                feature_label='feat_label', feature_select='feat_select')
+    testee.layout_extra_fields_topline(win, layout)
+    assert capsys.readouterr().out == (
+            "called BoxSizer.__init__ with args ('',)\n"
+            "called BoxSizer.__init__ with args (4,)\n"
+            "called hori sizer.Add with args <item>"
+            f" (0, {testee.wx.ALIGN_CENTER_VERTICAL | testee.wx.LEFT | testee.wx.RIGHT}, 2)\n"
+            "called hori sizer.Add with args <item>"
+            f" (0, {testee.wx.ALIGN_CENTER_VERTICAL | testee.wx.RIGHT}, 2)\n"
+            "called hori sizer.Add with args <item>"
+            f" (0, {testee.wx.ALIGN_CENTER_VERTICAL | testee.wx.LEFT | testee.wx.RIGHT}, 2)\n"
+            "called hori sizer.Add with args <item>"
+            f" (0, {testee.wx.ALIGN_CENTER_VERTICAL | testee.wx.RIGHT}, 2)\n"
+            "called hori sizer.Add with args <item>"
+            f" (0, {testee.wx.ALIGN_CENTER_VERTICAL | testee.wx.LEFT | testee.wx.RIGHT}, 2)\n"
+            "called hori sizer.Add with args <item>"
+            f" (0, {testee.wx.ALIGN_CENTER_VERTICAL | testee.wx.RIGHT}, 2)\n"
+            f"called  sizer.Add with args <item> (0, {testee.wx.LEFT}, 2)\n")
