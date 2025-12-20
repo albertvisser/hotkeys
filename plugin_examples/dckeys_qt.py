b8bed15745d381492ca50ca70b95c33d1e5712a0
Albert Visser <albert.visser@gmail.com>
2025-12-20 15:17:41 +0100

    kleine layout aanpassingen

--- plugin_examples/dckeys_qt.py
+++ plugin_examples/dckeys_qt.py
@@ -6,14 +6,11 @@ import PyQt6.QtWidgets as qtw
 def layout_extra_fields(win, layout):
     """add the extra fields to the layout
     """
-    sizer2 = qtw.QGridLayout()
+    sizer = qtw.QGridLayout()
     line = 0
-    sizer2.addWidget(win.lbl_parms, line, 0)
-    sizer2.addWidget(win.txt_parms, line, 1)
+    sizer.addWidget(win.lbl_parms, line, 0)
+    sizer.addWidget(win.txt_parms, line, 1)
     line += 1
-    sizer2.addWidget(win.lbl_controls, line, 0)
-    sizer3 = qtw.QHBoxLayout()
-    sizer3.addWidget(win.cmb_controls)
-    sizer3.addStretch()
-    sizer2.addLayout(sizer3, line, 1)
-    layout.addLayout(sizer2, 1)
+    sizer.addWidget(win.lbl_controls, line, 0)
+    sizer.addWidget(win.cmb_controls, line, 1)
+    layout.addLayout(sizer, 1)
--- plugin_examples/dckeys_wx.py
+++ plugin_examples/dckeys_wx.py
@@ -1,4 +1,4 @@
-"""Hotkeys plugin voor Double Commander - qt specifieke code
+"""Hotkeys plugin voor Double Commander - wx specifieke code
 """
 import wx
 
@@ -6,11 +6,11 @@ import wx
 def layout_extra_fields(win, layout):
     """add the extra fields to the layout
     """
-    sizer2 = wx.FlexGridSizer(2, 2, 2)
+    sizer = wx.FlexGridSizer(2, 2, 2)
     # line = 0
-    sizer2.Add(win.lbl_parms, 0, wx.ALIGN_CENTER_VERTICAL)
-    sizer2.Add(win.txt_parms, 1, wx.EXPAND)
+    sizer.Add(win.lbl_parms, 0, wx.ALIGN_CENTER_VERTICAL)
+    sizer.Add(win.txt_parms, 1, wx.EXPAND)
     # line += 1
-    sizer2.Add(win.lbl_controls, 0, wx.ALIGN_CENTER_VERTICAL)
-    sizer2.Add(win.cmb_controls, 1, wx.EXPAND)
-    layout.Add(sizer2, 0, wx.LEFT, 5)
+    sizer.Add(win.lbl_controls, 0, wx.ALIGN_CENTER_VERTICAL)
+    sizer.Add(win.cmb_controls, 1, wx.EXPAND)
+    layout.Add(sizer, 0, wx.LEFT, 5)
