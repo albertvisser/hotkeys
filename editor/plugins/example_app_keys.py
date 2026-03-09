dd39a01f9c262f1ac15e6dec8a7c55aa6265e921
Albert Visser <albert.visser@gmail.com>
2026-03-09 11:51:27 +0100

    2026-0004 eerste poging: tekst wat leesbaarder prberen te maken

--- editor/plugins/example_app_keys.py
+++ editor/plugins/example_app_keys.py
@@ -7,19 +7,19 @@ this module contains the general, non gui-toolkit specific part of the code
 # from example_app_keys_gui import layout_extra_fields_*  # om dit beschikbaar te maken voor import
 
 # uncomment this to define a routine to (re)build the csv file from source data
-# showinfo is a switch for in case you want to show instructions
-# otherstuff can be used to pass on things like a list of possible commands
-# to method add_extra_attributes
 # ** corresponds to the Rebuild_data setting **
+# - showinfo is a switch for in case you want to show instructions
+# - otherstuff can be used to pass on things like a list of possible commands
+#   to method add_extra_attributes
 # def build_data(parent, showinfo=True):
 #     shortcuts = collections.defaultdict()
 #     otherstuff = {}
 #     ...implement some logic here...
 #     return shortcuts, otherstuff
 
-# uncomment these to define routines to be used by methods of a HotKeyPanel instance
+# uncomment these to define a routine to extract nonstandard attributes
 # ** corresponds to the ShowDetails setting **
-# win is a reference to the HotKeyPanel instance
+# - win is a reference to the HotKeyPanel instance
 # def add_extra_attributes(win):
 #     """add attributes specific to this plugin
 #     e.g a shorter name for a collection read from build_data's otherstuff
